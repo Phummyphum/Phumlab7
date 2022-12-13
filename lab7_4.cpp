2f6afdafb825b06fd337c8dff59870b54017a9c1
phum <phumrapee.phum@gmail.com>
2022-12-13 16:35:17 +0700

    lab7.4 complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -10,6 +10,18 @@ int main(){
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
     //Write your code here.
-		
+	double x;
+	cout << "Please input GPA: ";
+	cin >> x;
+	int n=0;
+	int i=0;
+	while(n < 10){
+		if(x <= gpa[i]){
+		cout << id[i] << " " << name[i] << " " << gpa[i] << "\n"; 
+		}
+		i++ ;
+		n++ ;
+	}
+	
 	return 0;
 }
